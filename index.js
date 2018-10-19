425b830c7b79a502a03180279b613f3e4ec8166e
Grant Timmerman <timmerman+devrel@google.com>
2018-10-18 19:09:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ console.log('hi');
 var s = JSON.stringify(p);
 var parsed = JSON.parse(s);
 console.dir(parsed);
+/**
+
+The result will be something like this. See  type=FunctionDeclaration use body.id.name for the name.
+
+{ type: 'Program',
+  start: 0,
+  end: 82,
+  body:
+   [ { type: 'FunctionDeclaration',
+       start: 2,
+       end: 48,
+       id: [Object],
+       expression: false,
+       generator: false,
+       async: false,
+       params: [Array],
+       body: [Object] },
+     { type: 'VariableDeclaration',
+       start: 50,
+       end: 61,
+       declarations: [Array],
+       kind: 'var' },
+     { type: 'ExpressionStatement',
+       start: 62,
+       end: 80,
+       expression: [Object] } ],
+  sourceType: 'script' }
+
+**/
